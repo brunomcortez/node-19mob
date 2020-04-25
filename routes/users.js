f75a0caddb8c488dbe98129d6b42b054c22a17d4
Bruno Cortez <bruno.mcortez@gmail.com>
2020-04-25 09:06:30 -0300

    Controller

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+// controller
+const Users = require('../controller/Users');
+const usersController = new Users();
+
 // instancia do router
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
-    res.send(`Eu recebi o parametro ${req.params.id}`);
-});
+router.get('/:id', usersController.get);
 
 // export default router;
 module.exports = router;
